cf1fd653a1a1e9cb6f8bbd4a17af3ebc225162dd
devkwonms <alstj1379@nate.com>
2022-12-30 02:09:28 +0900

    setting

---
+++ src/test/java/com/project/soccer/SoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.soccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
