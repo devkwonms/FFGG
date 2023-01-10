9ded714c2032f52571209f6017319427a9755772
devkwonms <alstj1379@nate.com>
2023-01-11 04:44:42 +0900

    유저조회 후 유저닉네임, 레벨 가져오기(미완)

---
+++ src/main/java/com/project/soccer/controller/ApiController.java
@@ -0,0 +1,22 @@
+package com.project.soccer.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiController {
+
+    @GetMapping("/user")
+    public String userSelect(){
+
+        StringBuffer result = new StringBuffer();
+        String JsonPrintString = null;
+
+        String apiUrl = "";
+
+        return "ok";
+
+    }
+}
