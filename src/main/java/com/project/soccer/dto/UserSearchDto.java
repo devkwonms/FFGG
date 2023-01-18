b1c1ae58238b5b8e75802f2ff83e94535f9a5100
devkwonms <alstj1379@nate.com>
2023-01-18 17:52:38 +0900

    UserSearchDto 수정

--- src/main/java/com/project/soccer/dto/UserSearchDto.java
+++ src/main/java/com/project/soccer/dto/UserSearchDto.java
@@ -14,8 +14,4 @@ public class UserSearchDto {
     private String accessId;    // 유저고유식별자
     private int level;          // 유저레벨
 
-    private int matchType;      // 매치종류
-    private int division;       // 등급(티어)
-    private String achievementDate; // 등급(티어)경신날짜
-
 }
