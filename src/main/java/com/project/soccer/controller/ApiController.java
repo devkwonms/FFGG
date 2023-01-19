9b59daaf1253d8d7a90a34e6bde7f1a59ddf52b0
devkwonms <alstj1379@nate.com>
2023-01-19 22:24:35 +0900

    test

--- src/main/java/com/project/soccer/controller/ApiController.java
+++ src/main/java/com/project/soccer/controller/ApiController.java
@@ -1,22 +1,19 @@
 package com.project.soccer.controller;
 
+import com.project.soccer.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class ApiController {
 
-//    @GetMapping("/user")
-//    public String userSelect(){
-//
-//        StringBuffer result = new StringBuffer();
-//        String JsonPrintString = null;
-//
-//        String apiUrl = "";
-//
-//        return "ok";
-//
-//    }
+    @RequestMapping("/api/user")
+    @ResponseBody
+    public User getUser(){
+        return new User("user@email.com","user name");
+    }
 }
