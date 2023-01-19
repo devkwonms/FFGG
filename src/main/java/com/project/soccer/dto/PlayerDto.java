4b3429fe269de8142b7303fe1e36e4fd6f802121
devkwonms <alstj1379@nate.com>
2023-01-19 20:21:40 +0900

    PlayerDto 생성

---
+++ src/main/java/com/project/soccer/dto/PlayerDto.java
@@ -0,0 +1,17 @@
+package com.project.soccer.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerDto {
+
+    private Integer spId;         // 선수 고유 식별자 (/metadata/spid API 참고)
+    private Integer spPosition;   // 선수 포지션 (/metadata/spposition API 참고)
+    private Integer spGrade;      // 선수 강화 등급
+    private StatusDto status;     // 선수 경기 스탯
+}
