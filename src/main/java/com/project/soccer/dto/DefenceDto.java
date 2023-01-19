d9ee1a9eb1c567558db7867e420d79a28fb701c7
devkwonms <alstj1379@nate.com>
2023-01-19 20:20:03 +0900

    DefenceDto 생성

---
+++ src/main/java/com/project/soccer/dto/DefenceDto.java
@@ -0,0 +1,17 @@
+package com.project.soccer.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DefenceDto {
+
+    private Integer blockTry;       // 블락 시도 수
+    private Integer blockSuccess;   // 블락 성공 수
+    private Integer tackleTry;      // 태클 시도 수
+    private Integer tackleSuccess;  // 태클 성공 수
+}
