df05f4fa3caadc679cc9f688cd991644a5b979ca
devkwonms <alstj1379@nate.com>
2023-01-20 01:41:39 +0900

    UrlConn메소드 분기

--- src/main/java/com/project/soccer/service/UserSearchService.java
+++ src/main/java/com/project/soccer/service/UserSearchService.java
@@ -5,6 +5,7 @@ import com.project.soccer.dto.UserSearchDto;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedInputStream;
@@ -23,13 +24,16 @@ import java.util.Map;
 @Service
 public class UserSearchService {
 
+    @Autowired
+    private UrlConnService urlConnService;
+
     public UserSearchDto userSearchApi (UserSearchDto userSearchDto,String userName) throws UnsupportedEncodingException {
 
         userSearchDto.setNickName(userName);
 
         // 유저 닉네임으로 유저 정보 조회 API
         String userSearchApi = "https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=" + URLEncoder.encode(userName,"UTF-8");
-        String userSearchResult = urlConn(userSearchApi);
+        String userSearchResult = urlConnService.urlConn(userSearchApi);
 
         JSONObject userSearchJson = new JSONObject(userSearchResult);
 
@@ -48,7 +52,7 @@ public class UserSearchService {
         List<Map<String,Object>> topTierList = new ArrayList<>();
 
         String topTierApi = "https://api.nexon.co.kr/fifaonline4/v1.0/users/"+accessId+"/maxdivision";
-        String topTierResult = urlConn(topTierApi);
+        String topTierResult = urlConnService.urlConn(topTierApi);
 
         JSONArray topTierJson = new JSONArray(topTierResult);
         log.info("topTierJson  = {}", topTierJson);
@@ -71,29 +75,5 @@ public class UserSearchService {
         log.info("topTierList = {}", topTierList);
 
         return topTierList;
-    }
-    // api urlConn method
-    private String urlConn(String api) {
-        StringBuffer result = new StringBuffer();
-        System.setProperty("https.protocols", "TLSv1.2");
-
-        try {
-
-            URL url = new URL(api);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestProperty("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE1MjY5MjMzNzAiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4ODg4ODQyMSwiaWF0IjoxNjczMzM2NDIxLCJuYmYiOjE2NzMzMzY0MjEsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.SqVNR_woA6kakkt-DtclOu0DP5tJNwgd5q1DDqTde_Q");
-            urlConnection.connect();
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
-            String returnLine;
-
-            while((returnLine = bufferedReader.readLine()) != null) {
-                result.append(returnLine);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result.toString();
     }
 }
