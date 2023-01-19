fdd8e4bbff27f9d49b6be43887ca277d69d79a3d
devkwonms <alstj1379@nate.com>
2023-01-19 20:20:56 +0900

    MatchInfoDto 생성

---
+++ src/main/java/com/project/soccer/dto/MatchInfoDto.java
@@ -0,0 +1,26 @@
+package com.project.soccer.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MatchInfoDto {
+
+    private String accessId;                    // 유저 고유 식별자
+    private String Nickname;                    // 유저 닉네임
+    private MatchDetailDto matchDetail;         // 매치 결과 상세 정보
+    private ShootDto shoot;                     // 슈팅 정보
+    private List<ShootDetailDto> shootDetail;   // 슈팅 별 상세정보 리스트
+    private PassDto pass;                       // 패스 정보
+    private DefenceDto defence;                 // 수비 정보
+    private List<PlayerDto> player;             // 경기 사용 선수 정보
+
+
+
+}
