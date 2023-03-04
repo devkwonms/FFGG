5ca442c29d4bcd1cc979a7c314bbddc4d57760cf
devkwonms <alstj1379@nate.com>
2023-03-04 19:02:36 +0900

    getResult 생성(가독성을 위한 method생성)

--- backend/src/main/java/com/project/soccer/dto/MatchDto.java
+++ backend/src/main/java/com/project/soccer/dto/MatchDto.java
@@ -3,6 +3,7 @@ package com.project.soccer.dto;
 import lombok.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -16,4 +17,9 @@ public class MatchDto {
     private Integer matchType;  // 매치 종류
 
     private List<MatchInfoDto> matchInfo;   // 매치 참여 플레이어별 매치 내용 상세 리스트
+
+    public String getResult(MatchDto matchDto, int index) {
+        String matchResult = matchDto.getMatchInfo().get(index).getMatchDetail().getMatchResult();
+        return matchResult;
+    }
 }
