a3314d9c387fc7152b01a9ce49a0a1bc2aab63ca
devkwonms <alstj1379@nate.com>
2023-03-09 04:10:06 +0900

    등급(티어)이미지 url 필드 추가

--- backend/src/main/java/com/project/soccer/dto/TopTierDto.java
+++ backend/src/main/java/com/project/soccer/dto/TopTierDto.java
@@ -12,5 +12,6 @@ public class TopTierDto {
     private int matchType;      // 매치종류
     private int division;       // 등급(티어)
     private String achievementDate; // 등급(티어)경신날짜
+    private String divisionImgUrl;     // 등급(티어)이미지 url
 
 }
