6118f683e9de254dfcd40b8695d065de2324bcac
devkwonms <alstj1379@nate.com>
2023-03-08 23:35:09 +0900

    Router setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return (
-    <Home/>
-  );
+  return <Router>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user/:nickname" element={<Detail />} />
+    </Routes>
+</Router>;
 }
 
 export default App;
