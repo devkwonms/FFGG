97fb0c70c89b9850c02c5111d951b034b06d5363
devkwonms <alstj1379@nate.com>
2023-03-11 19:15:38 +0900

    try catch문 수정

--- backend/src/main/java/com/project/soccer/service/UrlConnService.java
+++ backend/src/main/java/com/project/soccer/service/UrlConnService.java
@@ -23,15 +23,18 @@ public class UrlConnService {
             URL url = new URL(api);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestProperty("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE1MjY5MjMzNzAiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY4ODg4ODQyMSwiaWF0IjoxNjczMzM2NDIxLCJuYmYiOjE2NzMzMzY0MjEsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.SqVNR_woA6kakkt-DtclOu0DP5tJNwgd5q1DDqTde_Q");
+            // connect()를 호출하여 서버로 요청을 보냄
             urlConnection.connect();
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"));
+
+            try(BufferedInputStream bufferedInputStream = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream, "UTF-8"))){
+
             String returnLine;
 
             while((returnLine = bufferedReader.readLine()) != null) {
                 result.append(returnLine);
             }
-
+          }
         } catch (Exception e) {
             e.printStackTrace();
         }
