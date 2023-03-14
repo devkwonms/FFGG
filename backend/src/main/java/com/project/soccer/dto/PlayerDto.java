6c028a27795410bd88addc51cc65f4f122a964ec
devkwonms <alstj1379@nate.com>
2023-03-14 19:03:19 +0900

    선수 이미지url 필드 추가(spImgUrl)

--- backend/src/main/java/com/project/soccer/dto/PlayerDto.java
+++ backend/src/main/java/com/project/soccer/dto/PlayerDto.java
@@ -14,5 +14,7 @@ public class PlayerDto {
     private int spPosition = 0;   // 선수 포지션 (/metadata/spposition API 참고)
     private int spGrade;      // 선수 강화 등급
     private StatusDto status;     // 선수 경기 스탯
+    private String spImgUrl;    // 선수 이미지 url
+
 
 }
