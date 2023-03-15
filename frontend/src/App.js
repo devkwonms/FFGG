d6a9f39319ea91cbc0e945f68c7bbedd3f710a01
devkwonms <alstj1379@nate.com>
2023-03-15 23:04:30 +0900

    route 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import Error from "./components/Error";
+import Navigation from "./components/Navigation";
+import GameList from "./components/GameList";
+import SoccerField from "./components/field/SoccerField";
 
 function App() {
   return <Router>
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user/:nickname" element={<Detail />} />
+        <Route path="/error" element={<Error/>} />
+        {/* <Route path="/navi" element={<Navigation/>} /> */}
+        {/* <Route path="/list" element={<GameList />} /> */}
+        <Route path="/field" element={<SoccerField />} />
     </Routes>
 </Router>;
 }
