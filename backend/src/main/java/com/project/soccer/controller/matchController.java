c6977e94d3732beda8da49b183e725a070f2fa20
devkwonms <alstj1379@nate.com>
2023-03-21 17:38:26 +0900

    getMatchId 추출하는 api 생성(미완)

--- backend/src/main/java/com/project/soccer/controller/matchController.java
+++ backend/src/main/java/com/project/soccer/controller/matchController.java
@@ -2,6 +2,7 @@ package com.project.soccer.controller;
 
 import com.project.soccer.dto.MatchDto;
 import com.project.soccer.dto.MatchThumbnailDto;
+import com.project.soccer.dto.MatchType;
 import com.project.soccer.dto.UserSearchDto;
 import com.project.soccer.service.MatchService;
 import com.project.soccer.service.UserSearchService;
@@ -41,16 +42,30 @@ public class matchController {
         Map<String,Object> resultMap = new HashMap<>();
 
         UserSearchDto userSearchDto = new UserSearchDto();
+
         userSearchDto = userSearchService.getUserInfo(userSearchDto, userName);
 
         List<Map<String,Object>> topTierList = userSearchService.getTopRank(userSearchDto.getAccessId());
-        List<MatchThumbnailDto> matchThumbnailList = matchService.getMatchThumbnail(userSearchDto.getAccessId());
+
+//        Map<String,List> matchInfoMap = matchService.getMatchId(userSearchDto.getAccessId(), matchtype, offset);
+
+//        List<Integer> matchTypeList = matchService.getMatchType();
+//        List<MatchThumbnailDto> matchThumbnailList = matchService.getMatchThumbnail(userSearchDto.getAccessId(), matchtype, limit, offset);
 
         resultMap.put("userSearchDto", userSearchDto);
         resultMap.put("topTierList", topTierList);
-        resultMap.put("matchThumbnailList", matchThumbnailList);
+//        resultMap.put("matchThumbnailList", matchThumbnailList);
+//        resultMap.put("matchTypeList", matchTypeList);
 
         return resultMap;
+    }
+    @ResponseBody
+    @GetMapping("/matches/{accessId}/{matchtype}/{offset}")
+    public Map<String,List> getMatchId(@PathVariable String accessId,@PathVariable int matchtype, @PathVariable int offset) throws IOException {
+
+        Map<String,List> matchInfoMap = matchService.getMatchId(accessId, matchtype, offset);
+
+        return matchInfoMap;
     }
 
     @ResponseBody
