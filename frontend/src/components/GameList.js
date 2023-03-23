f94e856c173e248564914bba972f5928f038c50b
devkwonms <alstj1379@nate.com>
2023-03-23 23:01:50 +0900

    GameList.js 1. Tab 클릭할 때마다  matchType별로 매치 10개씩 불러오기

--- frontend/src/components/GameList.js
+++ frontend/src/components/GameList.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import {Box, Container, Divider, Stack, Tab, Tabs} from "@mui/material"
 
 import Footer from "../components/Footer";
-import MatchThumbnailList from "../components/MatchThumbnailList";
+import MatchThumbnail from "./MatchThumbnail";
 import Navigation from '../components/Navigation';
 import UserInfo from '../components/UserInfo';
 
@@ -18,34 +18,34 @@ function GameList({accessId}){
     const requestOptions = {
         method: 'GET'
     };
-    const json = await (await fetch(`/matches/${accessId}/${matchType}/${offset}`, requestOptions)).json();
-    console.log(json);
+    const json = await (await fetch(`/api/matches/${accessId}/${matchType}/${offset}`, requestOptions)).json();
+  
     setList(json);
   };
-  
+
   useEffect(()=>{
     getMatchList(matchType);
   },[matchType]);
-
+  
   return(
   <div>
       
       <hr />
       <Box>
       <Tabs value={matchType} onChange={(e, newMatchType)=>{setMatchType(newMatchType)}} aria-label="basic tabs example">
-        <Tab label="공식경기" value={matchType} />
+        <Tab label="공식경기" value={50} />
         <Tab label="친선경기" value={40} />
         <Tab label="감독모드" value={52} />
       </Tabs>
-        {list?.matchThumbnailList?.map((info) => (
-            <MatchThumbnailList
+        {list?.matchDetailList?.map((info) => (
+            <MatchThumbnail
             key={info?.matchId}
             matchDate={info?.matchDate.substring(0,16).replace("T"," ")}
-            myResult={info?.myResult}
-            myNickName={info?.myNickName}
-            anotherNickname={info?.anotherNickname}
-            myGoal={info?.myGoal}
-            anotherGoal={info?.anotherGoal}
+            myResult={accessId === info?.matchInfo[0].accessId ? info?.matchInfo[0]?.matchDetail?.matchResult:info?.matchInfo[1]?.matchDetail?.matchResult}
+            leftNickName={info?.matchInfo[0]?.nickname}
+            rightNickname={info?.matchInfo[1]?.nickname}
+            leftScore={info?.matchInfo[0]?.shoot?.goalTotal}
+            rightScore={info?.matchInfo[1]?.shoot?.goalTotal}
             />
         ))}
         </Box>
