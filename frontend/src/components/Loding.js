4e8afd148d56f89302c4a1cda5fcab30ce8de191
devkwonms <alstj1379@nate.com>
2023-04-11 21:33:22 +0900

    loading component

---
+++ frontend/src/components/Loding.js
@@ -0,0 +1,14 @@
+import '../components/Loding.css';
+
+function Loding(){
+
+    return(
+        <div class="spinner-box">
+            <div class="circle-border">
+                <div class="circle-core">
+                </div>
+            </div>  
+        </div>
+    )
+}
+export default Loding;
