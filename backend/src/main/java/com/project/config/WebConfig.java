5b07f7237e2f2b4eec8a73077ef390135dc8b089
devkwonms <alstj1379@nate.com>
2023-08-30 03:52:08 +0900

    Crossorigin지정

--- backend/src/main/java/com/project/config/WebConfig.java
+++ backend/src/main/java/com/project/config/WebConfig.java
@@ -2,17 +2,15 @@ package com.project.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("*")
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
 //                .maxAge(3000);
     }
