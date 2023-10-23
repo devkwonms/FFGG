6ad68ab0f29bfed3e7f839efb75f6e45ea1abe21
devkwonms <alstj1379@nate.com>
2023-10-23 23:26:32 +0900

    들여쓰기, 문법에 맞게 수정

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,67 +1,65 @@
-import {useEffect, useReducer, useState} from "react";
-import styled from 'styled-components';
-import { Link, useNavigate } from 'react-router-dom';
-import bgImg from '../image/messi.jpg';
-import Navigation from '../components/Navigation';
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"></link>
+import { useEffect, useReducer, useState } from "react";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import bgImg from "../image/messi.jpg";
+import Navigation from "../components/Navigation";
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"></link>;
 
 const Home = () => {
   const navigate = useNavigate();
   const [input, setInput] = useState("delariva");
-  const reset = ()=> {
+  const reset = () => {
     setInput("");
-}
-const onChangeInput = (e) =>{
-  setInput(e.target.value);
-}
-console.log(input);
+  };
+  const onChangeInput = (e) => {
+    setInput(e.target.value);
+  };
+  console.log(input);
 
   const handleOnClick = (input) => {
     navigate(`/user/${input}`);
   };
-const handleOnKeyPress = e => {
-  if (e.key === 'Enter') {
-    console.log(e.key);
-    handleOnClick(input);
-  }
+  const handleOnKeyPress = (e) => {
+    if (e.key === "Enter") {
+      console.log(e.key);
+      handleOnClick(input);
+    }
+  };
+
+  return (
+    <HomeContainer>
+      <Navigation />
+      <FormContainer>
+        <Form>
+          <Input
+            value={input}
+            onChange={onChangeInput}
+            placeholder="구단주명을 입력해주세요!"
+            type="text"
+            onKeyPress={handleOnKeyPress}
+          />
+          <Button onClick={reset}>Reset</Button>
+          <Link to={`/user/${input}`}>
+            <Button>조회</Button>
+          </Link>
+        </Form>
+      </FormContainer>
+    </HomeContainer>
+  );
 };
-  
-    return (
-        <HomeContainer>
-          <Navigation/>
-            <FormContainer>
-                <Form>
-                    <Input
-                      value={input}
-                      onChange={onChangeInput}
-                      placeholder="구단주명을 입력해주세요!"
-                      type="text"
-                      onKeyPress={handleOnKeyPress}
-                    />
-                    <Button onClick={reset}>Reset</Button>
-                    <Link to ={`/user/${input}`}>
-                    <Button >조회</Button>
-                    </Link>
-                </Form>
-            </FormContainer>
-        </HomeContainer>
-    )
-  
-}
 
 export default Home;
 
-
 const HomeContainer = styled.div`
-  background-image: url('${bgImg}');
+  background-image: url("${bgImg}");
   background-repeat: no-repeat;
   background-position: top center;
   background-size: cover;
   background-attachment: fixed;
   height: 100vh;
   margin: 0 auto;
-  opacity : 0.9;
-`
+  opacity: 0.9;
+`;
 // logo img
 const HomeMainImg = styled.img`
   max-width: 360px;
@@ -69,13 +67,13 @@ const HomeMainImg = styled.img`
   margin: 0 auto;
   margin-left: 1rem;
   margin-top: 2rem;
-`
+`;
 
 const FormContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Form = styled.form`
   width: 664px;
@@ -86,9 +84,9 @@ const Form = styled.form`
   overflow: hidden;
   padding: 0px;
   border-radius: 8px;
-  border: 1px solid #48526A;
+  border: 1px solid #48526a;
   background-color: white;
-`
+`;
 
 const Input = styled.input`
   outline: none;
@@ -108,29 +106,29 @@ const Input = styled.input`
   border-style: none;
   border-color: initial;
   border-image: initial;
-`
+`;
 
 const Button = styled.button`
-display: flex;
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 10px 10px 0 0;
-    height: 30px;
-    padding: 0;
-    border: none;
-    color: #141D33;
-    width: 70px;
-    font-size: 18px;
-    font-weight: 600;
-    background-color: #C8CFDB;;
-    justify-content: center;
-    outline: none;
-    border-radius: 5px;
-`
+  display: flex;
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 10px 10px 0 0;
+  height: 30px;
+  padding: 0;
+  border: none;
+  color: #141d33;
+  width: 70px;
+  font-size: 18px;
+  font-weight: 600;
+  background-color: #c8cfdb;
+  justify-content: center;
+  outline: none;
+  border-radius: 5px;
+`;
 
 const LogoText = styled.h3`
-padding-left: 2%;
-color : white;
-font-family: 'Montserrat', sans-serif;
-`
+  padding-left: 2%;
+  color: white;
+  font-family: "Montserrat", sans-serif;
+`;
